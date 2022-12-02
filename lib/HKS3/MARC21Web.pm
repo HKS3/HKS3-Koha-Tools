fdc19b4434db140e87d7565aca1769f47349d5f1
Mark Hofstetter <mark@hofstetter.at>
2022-12-02 11:28:57 +0100

    updated to correct k10plus link

--- lib/HKS3/MARC21Web.pm
+++ lib/HKS3/MARC21Web.pm
@@ -5,18 +5,10 @@ use warnings;
 use Encode;
 use feature qw/say/;
 use POSIX qw();
+use Exporter 'import';
 use XML::XPath;
-use Exporter qw/import/;
 
-our @EXPORT_OK = qw/
-                     get_marc_via_id
-                     get_empty_record
-                     get_empty_auth_record
-                     marc_record_from_xml
-                     add_field
-                     get_marc_file
-                 /;
-
+our @EXPORT_OK = qw(get_marc_via_id);
 
 use List::Util qw/ any /;
 
@@ -27,9 +19,7 @@ use Getopt::Long;
 use DateTime;
 use Data::Dumper;
 use Encode qw(decode encode);
-use MARC::Record;
-use MARC::File::XML qw//;
-use MARC::Charset;
+
 
 my $web_resources = {
     'dnb' => {
@@ -41,7 +31,8 @@ my $web_resources = {
         xml => '//zs:searchRetrieveResponse/zs:records/zs:record/zs:recordData/record',
      },
     'k10p' => {
-        url => "https://sru.bsz-bw.de/swb?version=1.1&query=pica.%s=%s&operation=searchRetrieve&maximumRecords=10&recordSchema=marcxmlk10os",
+#         url => "https://sru.bsz-bw.de/swb?version=1.1&query=pica.%s=%s&operation=searchRetrieve&maximumRecords=10&recordSchema=marcxmlk10os",
+        url => "https://sru.k10plus.de/opac-de-627?version=1.1&operation=searchRetrieve&query=pica.%s=%s&maximumRecords=10&recordSchema=marcxml",
         xml => '//zs:searchRetrieveResponse/zs:records/zs:record/zs:recordData/record',
      },
 };
@@ -52,40 +43,32 @@ sub get_marc_via_id {
     my $cachedir = shift;
     my $sources = shift; # should be an arrayref [loc, dnb, ]
     my $xml;
-
-    if ( ! -d $cachedir ) {
-        die "not a directory: $cachedir";
-    }
-
+    
     # $isbn =~ s/.*\/(.*)/$1/g;
 
     foreach my $source (@$sources) {
 
-        if (! exists( $web_resources->{$source} ) ) {
-            die "Source '$source' not available. Use one of these: [" . join(', ', keys $web_resources->%*) . ']';
-        }
-
-        #print Dumper $web_resources->{$source};
+        print Dumper $web_resources->{$source};
         # in k10plus the search term for isbn is isb
-        if ($source eq 'k10p' && $type eq 'isbn') {
-            $type = 'isb';
+        if ($type eq 'isbn') {
+            $type = $source eq 'k10p' ? 'isb' : 'isbn';
         }
         my $filename  = sprintf("%s/%s-sru-export-%s-%s.xml", $cachedir, $source, $type, $id);
-        printf("%s \n", $filename);
+        printf("%s \n", $filename);        
 
         if (-f $filename) {
             $xml = path($filename)->slurp_utf8;
-            printf("file length %d \n", length($xml));
+            printf("length %d \n", length($xml));
             next if length($xml) == 0;
             return $xml;
         }
-
+    
         # printf("Source %s\n", $source->{name});
         my $url = sprintf($web_resources->{$source}->{url}, $type, $id);
         $xml = fetch_marc_from_url($url, $filename, $web_resources->{$source}->{xml});
         sleep(5);
         return $xml if $xml &&  length($xml) > 0;
-    }
+    }    
 
     return $xml;
 }
@@ -94,9 +77,9 @@ sub fetch_marc_from_url {
     my $url = shift;
     my $filename = shift;
     my $record_node = shift;
-
-
-    print "URL $url \n";
+        
+    
+    print "$url \n";
     my $req = HTTP::Request->new(GET => $url);
     my $ua = LWP::UserAgent->new;
     $ua->default_header(
@@ -112,22 +95,20 @@ sub fetch_marc_from_url {
     my $response = $ua->request($req);
     if ($response->is_success) {
         my $xp = XML::XPath->new(xml => $response->content);
-        #printf("Record Node: %s \n", $record_node);
+        printf("%s \n", $record_node);
         my $nodeset = $xp->find($record_node);
         my $xml;
         foreach my $node ($nodeset->get_nodelist) {
             $xml = XML::XPath::XMLParser::as_string($node);
             last;
         }
-        $xml = '' if ! defined $xml;
+        # write_file($filename, {binmode => ':raw'}, $xml);
         # write_file($filename, {binmode => ':raw'}, $xml);
         path($filename)->spew_utf8($xml);
         return $xml;
     }
     else {
-        printf("Response %s\n", $response->code);
-        #use Data::Dumper;
-        #warn Dumper($response);
+            printf("Response %s\n", $response->code);
     }
 }
 
@@ -148,43 +129,5 @@ XML
 return $xml;
 }
 
-sub get_empty_auth_record {
-my $xml = <<XML;
-<?xml version="1.0" encoding="UTF-8"?>
-<record
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd"
-    xmlns="http://www.loc.gov/MARC21/slim">
-
-  <leader>00207nz  a2200109n  4500</leader>
-  <controlfield tag="005">20221016160626.0</controlfield>
-  <controlfield tag="008">221108|| aca||babn           | a|a     d</controlfield>
-</record>
-XML
-
-return $xml;
-}
-
-sub marc_record_from_xml {
-    my $xml = shift;
-    my $record = MARC::Record->new_from_xml( $xml, 'UTF-8', 'MARC21' );
-    return $record;
-}
-
-sub add_field {
-    my ($record, $field, $ind1, $ind2, $subfield, $value) = @_;
-    $record->append_fields(
-        MARC::Field->new( $field, $ind1, $ind2, $subfield, $value )
-    );
-}
-
-sub get_marc_file {
-    my $filename = shift;
-    die "output file exists. ($filename)" if -f $filename;
-
-    MARC::File::XML->default_record_format('MARC21');
-    my $file = MARC::File::XML->out( $filename, 'UTF-8' );
-    return $file;
-}
 
 1;
